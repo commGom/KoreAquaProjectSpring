49be2c01c6551110a0aa6d44ac2ebe1b026e2ee5
sokkung9 <csy1506@gmail.com>
2020-12-27 22:43:42 +0900

    admin, qna sql 추가

---
+++ src/main/webapp/WEB-INF/views/sql/qna.sql
@@ -0,0 +1,22 @@
+drop table qna purge;
+
+create table qna
+( num NUMBER(4) primary key,
+ title VARCHAR2(200) not null,
+ author VARCHAR2(20) not null,
+ email VARCHAR2(50) not null,
+ passwd VARCHAR2(20) not null,
+ content VARCHAR2(4000) not null,
+ filename VARCHAR2(50),
+ writeday DATE default SYSDATE,
+ readCnt NUMBER(4) default 0,
+ repRoot NUMBER(4),
+ repStep NUMBER(4),
+ repIndent NUMBER(4));
+
+drop sequence qna_seq; 
+create sequence qna_seq;
+
+insert into qna(num, title, author, email, passwd, content, filename, repRoot, repStep, repIndent)
+values(qna_seq.nextval, '테스트', '홍길동', 'hong@naver.com', 1234, '테스트입니다', null, qna_seq.currval, 0, 0);
+commit;
